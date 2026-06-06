c384743267e3e8381f4cbcc2372751f465950640
Krish Makadiya <krishmakadiya2005@gmail.com>
2026-06-06 13:46:35 +0530

    fix: add cleanup step before FK constraints in identity migration

--- drizzle/0010_identity_system_update.sql
+++ drizzle/0010_identity_system_update.sql
@@ -7,6 +7,10 @@ ALTER TABLE "likes" ADD CONSTRAINT "likes_userEmail_doubtId_unique" UNIQUE("user
 ALTER TABLE "reply_likes" DROP CONSTRAINT IF EXISTS "reply_likes_userName_replyId_unique";
 ALTER TABLE "reply_likes" ADD CONSTRAINT "reply_likes_userEmail_replyId_unique" UNIQUE("userEmail","replyId");
 
+-- Clean orphaned records before adding foreign key constraints
+DELETE FROM "likes" WHERE "userEmail" NOT IN (SELECT "email" FROM "users");
+DELETE FROM "reply_likes" WHERE "userEmail" NOT IN (SELECT "email" FROM "users");
+
 ALTER TABLE "likes" DROP CONSTRAINT IF EXISTS "likes_userName_users_email_fk";
 ALTER TABLE "likes" ADD CONSTRAINT "likes_userEmail_users_email_fk" FOREIGN KEY ("userEmail") REFERENCES "public"."users"("email") ON DELETE cascade ON UPDATE no action;
 
