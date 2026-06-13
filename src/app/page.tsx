bc693a4d0bfaf5297723eed4da4e6d8657d20f9e
Pheonix <150040105+ArshiBansal@users.noreply.github.com>
2026-06-13 13:41:58 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -304,7 +304,6 @@ export default function Home() {
           </div>
         </section>
 
-        {/* ... rest of your sections unchanged ... */}
         {/* Features Section */}
         <section
           id="features"
